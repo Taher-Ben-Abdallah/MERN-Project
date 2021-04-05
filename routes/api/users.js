908d366bbf7b49afd93d50bd108585cde5655991
Taher <you@example.commed.taher.benabdallah@gmail.com>
2021-04-05 17:43:01 +0100

    Login and Register completion

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,11 @@ const express= require('express');
 const router = express.Router();
 const gravatar=  require ('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require ('jsonwebtoken');
+const config = require ('config');
 const { check, validationResult} = require('express-validator');
 
 const User= require('../../models/User');
-//@route    GET api/users
-//@desc     Test route
-//@access   Public
-router.get('/',(req,res)=>res.send('User route'));
 
 //@route    POST api/users
 //@desc     Register route
@@ -34,7 +32,7 @@ router.post('/',[
         }
     //Get users gravatar
         const avatar= gravatar.url(email, {
-            s:'2',
+            s:'200',
             r: 'pg',
             d: 'mm'
         });
@@ -47,7 +45,20 @@ router.post('/',[
 
         await user.save();
     //Return jwt
-    res.send('User registered');
+    const payload= {
+        user: {
+            id: user.id,
+
+        }
+    }
+
+    jwt.sign(payload,config.get('jwtSecret'),{
+        expiresIn: 360000},
+        (err,token)=> {
+            if (err) throw err;
+            res.json({token})
+        }
+    );
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
